ab4b08b427634fd62a42d3567fc21096c0c11eee
MxnlodyX <wattanachai.work@hotmail.com>
2026-03-18 09:05:21 +0700

    fix : update route for field ops

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,39 @@
 import { AppProvider } from './context/AppContext';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { ProtectedRoute } from './components/auth/ProtectedRoute';
 import { BackOfficeLayout } from './pages/backoffice/BackOfficeLayout';
 import { DashboardPage } from './pages/backoffice/DashboardPage';
 import { HRMDashboard } from './pages/backoffice/HRM';
 import { InventoryPage } from './pages/backoffice/InventoryPage';
 import { PatrolAreasPage } from './pages/backoffice/PatrolAreasPage';
-import { ReportsPage } from './pages/backoffice/ReportsPage';
-import { SignInPage } from './pages/SignInPage';
+import { BackofficeSignInPage } from './pages/BackofficeSignInPage';
 import { TaskAssignPage } from './pages/backoffice/TaskAssignPage';
+import { FieldOpsHomePage } from './pages/field-ops/FieldOpsHomePage';
+import { FieldOpsSignInPage } from './pages/FieldOpsSignInPage';
 
 
 function App() {
   return (
     <AppProvider>
       <Routes>
-        <Route path="/" element={<SignInPage />} />
-        <Route element={<BackOfficeLayout />}>
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/hrm" element={<HRMDashboard />} />
-          <Route path="/inventory" element={<InventoryPage />} />
-          <Route path="/reports" element={<ReportsPage />} />
-          <Route path="/areas" element={<PatrolAreasPage />} />
-          <Route path="/taskassignment" element={<TaskAssignPage />} />
+        <Route path="/" element={<Navigate to="/signin/backoffice" replace />} />
+        <Route path="/signin/backoffice" element={<BackofficeSignInPage />} />
+        <Route path="/signin/fieldops" element={<FieldOpsSignInPage />} />
+
+        <Route element={<ProtectedRoute allowedRoles={['backoffice']} />}>
+          <Route element={<BackOfficeLayout />}>
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/hrm" element={<HRMDashboard />} />
+            <Route path="/inventory" element={<InventoryPage />} />
+            <Route path="/areas" element={<PatrolAreasPage />} />
+            <Route path="/taskassignment" element={<TaskAssignPage />} />
+          </Route>
         </Route>
-        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+
+        <Route element={<ProtectedRoute allowedRoles={['fieldops']} />}>
+          <Route path="/field-ops/home" element={<FieldOpsHomePage />} />
+        </Route>
+
       </Routes>
     </AppProvider>
   );
