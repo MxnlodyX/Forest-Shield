7443fabb853161fb1b344b177f46c489b5d6738d
MxnlodyX <wattanachai.work@hotmail.com>
2026-03-18 08:30:44 +0700

    fix : update routing split office side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,31 @@
 import { AppProvider } from './context/AppContext';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { BackOfficeLayout } from './pages/backoffice/BackOfficeLayout';
+import { DashboardPage } from './pages/backoffice/DashboardPage';
 import { HRMDashboard } from './pages/backoffice/HRM';
+import { InventoryPage } from './pages/backoffice/InventoryPage';
+import { PatrolAreasPage } from './pages/backoffice/PatrolAreasPage';
+import { ReportsPage } from './pages/backoffice/ReportsPage';
 import { SignInPage } from './pages/SignInPage';
-import { UsersPage } from './pages/UsersPage';
+import { TaskAssignPage } from './pages/backoffice/TaskAssignPage';
 
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <AppProvider>
       <Routes>
         <Route path="/" element={<SignInPage />} />
-        <Route path="/users" element={<UsersPage />} />
-        <Route path="/hrm" element={<HRMDashboard />} />
+        <Route element={<BackOfficeLayout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/hrm" element={<HRMDashboard />} />
+          <Route path="/inventory" element={<InventoryPage />} />
+          <Route path="/reports" element={<ReportsPage />} />
+          <Route path="/areas" element={<PatrolAreasPage />} />
+          <Route path="/taskassignment" element={<TaskAssignPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </AppProvider>
- 
   );
 }
 
